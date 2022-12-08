df2b260619a43829fad2c44f67aae695db2e4855
Pashanur Merdanov <pasanurmerdanov@gmail.com>
2022-12-08 16:51:51 +0400

    ..

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -84,3 +84,23 @@ $('.info-textarea').keyup(function() {
 //   }
 // }
 // window.addEventListener('load',UrlExists(url))
+
+
+
+//email validation
+function handleInput(evt) {
+  const value = evt.target.value
+  const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  
+  if (emailRegex.test(value.trim())) {
+      evt.target.classList.add('valid')
+      evt.target.classList.remove('invalid')
+  } else {
+      evt.target.classList.add('invalid')
+      evt.target.classList.remove('valid')
+  }
+
+  if (!value) {
+      evt.target.classList.remove('invalid')
+  }
+}
